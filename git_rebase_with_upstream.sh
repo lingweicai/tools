0cab345ec20cfbb5a7800e283fca9ff629e4a99a
Forrest Ling 凌巍才 <lingweicai@users.noreply.github.com>
2025-07-15 16:26:27 +0800

    Update git_rebase_with_upstream.sh

--- git_rebase_with_upstream.sh
+++ git_rebase_with_upstream.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# setup user name and email
+$ git config --global user.name "John Doe"
+$ git config --global user.email johndoe@example.com
+# git rebase
 git remote add upstream https://github.com/cockpit-project/starter-kit.git
 git fetch upstream
 git checkout main
